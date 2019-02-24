826ff39b2e2d1fe6e5cc134a8718e353cf82176f
sagodi97 <36556883+sagodi97@users.noreply.github.com>
2019-02-24 23:42:24 +0100

    fix path in server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express'), path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/santiangular'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/santiangular/index.html'))
 })
 
 console.log('Console listening papo!');
