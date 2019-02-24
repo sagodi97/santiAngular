a117a56f6881912c9a35cff94111407e4bebe3eb
sagodi97 <36556883+sagodi97@users.noreply.github.com>
2019-02-24 22:46:19 +0100

    Configure deployment settings for Heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'), path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
+})
+
+console.log('Console listening papo!');
